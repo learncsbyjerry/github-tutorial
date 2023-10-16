923dc690bc1f9f95fd43b30e1091a0088505afa2
learncsbyjerry <cs0219038@gmail.com>
2023-10-16 20:47:45 +0530

    Initial commit

---
+++ gitdemo.java
@@ -0,0 +1,9 @@
+public class gitdemo {
+      
+public static void main(String[] args) {
+      
+System.out.println("Code With Harry is invalid\r\n" + //
+            "Can contain alphabets, $character ... After that, \r\n" + //
+            "we need to analyze the min & max value we might use. Handwritten");
+}  
+}
