2bce310a6f7007a9d247c57a32860ab18f1669b2
learncsbyjerry <cs0219038@gmail.com>
2023-10-16 21:13:03 +0530

    Update gitdemo.java
    ho gaya copy re baba

--- gitdemo.java
+++ gitdemo.java
@@ -5,5 +5,9 @@ public static void main(String[] args) {
 System.out.println("Code With Harry is invalid\r\n" + //
             "Can contain alphabets, $character ... After that, \r\n" + //
             "we need to analyze the min & max value we might use. Handwritten");
-}  
+System.out.println("axPayCalculator.c\r\n" + //
+            "TemperatureCovertor.c\r\n" + //
+            "TernaryOperator.c\r\n" + //
+            "Variable.c");
+      }  
 }
